b288095c012afb9245ab9f0b8601e6f01c25a528
luvelut <luvelut@iutclinfb1910l.iut.local>
2021-02-07 11:00:44 +0100

    Ajout d'une activité avec un bouton camera qui accède à l'appli appareil photo

--- carnetVoyage/app/src/main/java/fr/iut/projet/view/CarnetActivity.java
+++ carnetVoyage/app/src/main/java/fr/iut/projet/view/CarnetActivity.java
@@ -2,22 +2,23 @@ package fr.iut.projet.view;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import fr.iut.projet.R;
 
-public class CreateActivity extends AppCompatActivity {
+public class CarnetActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.create_activity);
+        setContentView(R.layout.carnet_activity);
     }
 
-    public void clicBoutonValider(View sender){
-        Intent monIntent=new Intent(this, CarnetActivity.class);
+    public void clicBoutonCamera(View sender){
+        Intent monIntent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivity(monIntent);
     }
 }
