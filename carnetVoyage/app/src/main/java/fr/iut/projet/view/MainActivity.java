2d8dcfdc8ce60fac1614121c38667eeb0472b0a6
lorusak <loup.rusak@etu.uca.fr>
2021-02-11 11:39:17 +0100

    Ajout de la barre de navigation et de ses icones + règlage thème sombre

--- carnetVoyage/app/src/main/java/fr/iut/projet/view/MainActivity.java
+++ carnetVoyage/app/src/main/java/fr/iut/projet/view/MainActivity.java
@@ -6,16 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import fr.iut.projet.R;
 import fr.iut.projet.view.CreateActivity;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 
     public void clicBoutonCreate(View sender){
         Intent monIntent=new Intent(this, CreateActivity.class);
