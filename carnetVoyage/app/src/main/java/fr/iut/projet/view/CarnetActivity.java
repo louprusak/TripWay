d31f17beaa8a4dbab37bfd1aaa483a2a55bf16c7
luvelut <luvelut@iutclinfb1910l.iut.local>
2021-02-18 12:42:25 +0100

    Travail sur l'affichage d'une photo après l'avoir prise (bouton Camera) (ça marche pas)

--- carnetVoyage/app/src/main/java/fr/iut/projet/view/CarnetActivity.java
+++ carnetVoyage/app/src/main/java/fr/iut/projet/view/CarnetActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
@@ -13,13 +14,25 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import fr.iut.projet.R;
 
 public class CarnetActivity extends AppCompatActivity {
 
+    //constantes requestCode des Intent
+    private static final int RETOUR_GALERIE = 1;
+    private static final int RETOUR_CAMERA = 2;
+
     private ImageView imgPhoto;
     private Button btnPhoto;
+    private Button btnCamera;
+    private String photoPath = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +48,11 @@ public class CarnetActivity extends AppCompatActivity {
         //récupération des objets graphiques
         imgPhoto=(ImageView)findViewById(R.id.imgPhoto);
         btnPhoto=(Button)findViewById(R.id.btnGalerie);
+        btnCamera=(Button)findViewById(R.id.btnCamera);
 
         //initialisation du clic sur le bouton galerie
         createOnClicGalerie();
+        createOnClicCamera();
     }
 
 
@@ -51,7 +66,7 @@ public class CarnetActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 Intent galerieIntent=new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(galerieIntent,1);
+                startActivityForResult(galerieIntent,RETOUR_GALERIE);
             }
         });
     }
@@ -64,15 +79,15 @@ public class CarnetActivity extends AppCompatActivity {
      */
     public void onActivityResult(int requestCode,int resultCode,Intent data){
         super.onActivityResult(requestCode,resultCode,data);
-        //Vérification si une image est récupérée
-        if(requestCode==1 && resultCode==RESULT_OK){
+        //INTENT DE LA GALERIE
+        if(requestCode==1 && resultCode==RESULT_OK) {
 
             //Accès à l'image depuis l'intent
             Uri monImage = data.getData();
             String[] filePathColumn = {MediaStore.Images.Media.DATA};
 
             //Curseur pour accèder au chemin de l'image
-            Cursor curseur=this.getContentResolver().query(monImage,filePathColumn,null,null,null);
+            Cursor curseur = this.getContentResolver().query(monImage, filePathColumn, null, null, null);
 
             //Position sur la première ligne (normalement il y en a qu'une car une seule image)
             curseur.moveToFirst();
@@ -83,14 +98,18 @@ public class CarnetActivity extends AppCompatActivity {
             curseur.close();
 
             //Récupération de l'image
-            Bitmap image= BitmapFactory.decodeFile(imgPath);
+            Bitmap image = BitmapFactory.decodeFile(imgPath);
 
             //Affichage de l'image
             imgPhoto.setImageBitmap(image);
+        }
+        //INTENT DE LA CAMERA
+        if(requestCode==2 && resultCode==RESULT_OK){
+            Bitmap image = BitmapFactory.decodeFile(photoPath);
+            imgPhoto.setImageBitmap(image);
 
-        }else{
-            Toast.makeText(this, "Aucune image sélectionnée",Toast.LENGTH_LONG).show();
-        } //FAIRE UN TRY CATCH AU CAS OU IL Y A PAS LA PERMISSION D'ACCEDER AUX PHOTOS
+        }
+
     }
 
 
@@ -100,8 +119,44 @@ public class CarnetActivity extends AppCompatActivity {
         //TO DO : Créer un nouvelle activité qui s'ouvrira ici qui contiendra une textEdit
     }
 
-    public void clicBoutonCamera(View sender){
-        Intent monIntent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivity(monIntent);
+
+
+    private void createOnClicCamera(){
+        btnCamera.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                prendrePhoto();
+
+            }
+        });
     }
+
+    /**
+     * est appelée au onClick sur le bouton Camera
+     * méthode qui accède à l'appareil photo et mémorise dans un fichier temporaire
+     */
+    private void prendrePhoto(){
+        Intent monIntent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // test est ce que le téléphone a bien une caméra
+        if(monIntent.resolveActivity(getPackageManager()) != null) {
+            //créer un nom de fichier unique
+            String time = new SimpleDateFormat("yyyyMMdd_HHmmdd").format(new Date());
+            File photoDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+            try {
+                File photoFile = File.createTempFile("photo"+time,".jpg",photoDir);
+                //enregistrer le chemin complet
+                photoPath = photoFile.getAbsolutePath();
+                // créer l'URI
+                Uri photoUri = FileProvider.getUriForFile(CarnetActivity.this,CarnetActivity.this.getApplicationContext().getPackageName()+".provider",photoFile);
+                //transfert URI vers l'intent pour enregistrement dans fichier temporaire
+                monIntent.putExtra(MediaStore.EXTRA_OUTPUT,photoUri);
+                startActivityForResult(monIntent,RETOUR_CAMERA);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+
 }
